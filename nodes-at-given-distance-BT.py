8991d017e364e82362afa29b5019868819f91015
Harish <harishcse18501@gmail.com>
2020-09-22 18:39:37 +0530

    #day2

---
+++ nodes-at-given-distance-BT.py
@@ -0,0 +1,137 @@
+# Given a binary tree, a target node in the binary tree, and an integer value k, find all the nodes that are at distance k from the given target node. No parent pointers are available.
+
+# target- start node
+# k - position from target
+
+#           20
+#         /   \
+#       8      22 
+#     /  \
+#    4    12 
+#        /  \
+#       10   14
+
+# Target Node = 8
+# K = 2
+
+# Output: 10 14 22
+
+# Explanation: The three nodes at distance 2
+# from node 8 are 10, 14, 22.
+
+from collections import deque
+
+parentDict = {}
+resultList = []
+visited = []
+targetNode = None
+
+def createParentDict(root, target, prev):
+    if root: 
+        global parentDict, targetNode
+        if root.data == target:
+            targetNode = root
+        parentDict[root.data] = prev
+        prev = root
+        createParentDict(root.left, target, prev)
+        createParentDict(root.right, target, prev)
+
+def findNodesAtK(root, k):
+    global resultList, visited
+    if not root or k < 0 or root.data in visited:
+        return 
+    if k == 0 and root.data not in visited:
+        resultList.append(root.data)
+        return
+    visited.append(root.data)
+    findNodesAtK(root.left, k-1)
+    findNodesAtK(root.right, k-1)
+    if parentDict[root.data] != -1:
+        findNodesAtK(parentDict[root.data], k-1)
+
+     
+
+def KDistanceNodes(root,target,k):
+    global parentDict, visited, resultList
+    parentDict = {}
+    visited = []
+    resultList = []
+    createParentDict(root, target, -1)
+    findNodesAtK(targetNode, k)
+    return resultList
+
+# Tree Node
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+
+# Function to Build Tree
+def buildTree(s):
+    # Corner Case
+    if (len(s) == 0 or s[0] == "N"):
+        return None
+
+    # Creating list of strings from input
+    # string after spliting by space
+    ip = list(map(str, s.split()))
+
+    # Create the root of the tree
+    root = Node(int(ip[0]))
+    size = 0
+    q = deque()
+
+    # Push the root to the queue
+    q.append(root)
+    size = size + 1
+
+    # Starting from the second element
+    i = 1
+    while (size > 0 and i < len(ip)):
+        # Get and remove the front of the queue
+        currNode = q[0]
+        q.popleft()
+        size = size - 1
+
+        # Get the current node's value from the string
+        currVal = ip[i]
+
+        # If the left child is not null
+        if (currVal != "N"):
+            # Create the left child for the current node
+            currNode.left = Node(int(currVal))
+
+            # Push it to the queue
+            q.append(currNode.left)
+            size = size + 1
+        # For the right child
+        i = i + 1
+        if (i >= len(ip)):
+            break
+        currVal = ip[i]
+
+        # If the right child is not null
+        if (currVal != "N"):
+            # Create the right child for the current node
+            currNode.right = Node(int(currVal))
+
+            # Push it to the queue
+            q.append(currNode.right)
+            size = size + 1
+        i = i + 1
+    return root
+
+if __name__ == "__main__":
+    t = int(input("No. of test cases: ")) #testcases
+    for _ in range(t):
+        line = input("Tree list(level-order) :")
+        target=int(input("Target: "))
+        k=int(input("k: "))
+
+        root = buildTree(line)
+        res = KDistanceNodes(root,target,k)
+        
+        for i in res:
+            print(i, end=' ')
+        print()
