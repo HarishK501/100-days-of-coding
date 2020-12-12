1207e8e2e1910fac916c7bd6b54f08163c0a46f4
Harish K <60997526+HarishK501@users.noreply.github.com>
2020-12-12 18:59:27 +0530

    #day84

---
+++ move-zeros-end-array.py
@@ -0,0 +1,34 @@
+def moveZerosToEnd(A):
+    pivot = 0
+    j = 0
+
+    for i in range(len(A)):
+        if not A[i] == pivot:
+            tmp = A[i]
+            A[i] = A[j]
+            A[j] = tmp
+            j += 1
+
+    return
+
+
+def main():
+    print("Type # to exit.")
+    while True:
+        s = input("\nEnter array elements: ")
+        if s == "#":
+            break
+        arr = list(map(int, s.split()))
+        if 0 in arr:
+            moveZerosToEnd(arr)
+            print("Result:\n", arr)
+        else:
+            print("No zeros found in array.")
+        # longestBitonicSubarray(arr)
+
+    return "\nThank You\n"
+
+
+if __name__ == '__main__':
+
+    print(main())
